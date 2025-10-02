a3b223d8203d7ef9c34f717877057b1d509be8b4
𝐒𝐨𝐲𝕄𝕒𝕪𝕔𝕠𝕝 ᵒᶠⁱᶜⁱᵃˡ <karatekidamericatv@gmail.com>
2025-10-02 16:31:11 -0500

    Update main-owner.js

--- plugins/main-owner.js
+++ plugins/main-owner.js
@@ -1,9 +1,29 @@
-let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;Ado⁩;;\nFN:Ado\nORG:Ado\nTITLE:\nitem1.TEL;waid=50493732693:50493732693\nitem1.X-ABLabel:Ado\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:Ado\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'Ado', contacts: [{ vcard }] }}, {quoted: m})
-}
-handler.help = ['owner']
-handler.tags = ['main']
-handler.command = ['owner', 'creator', 'creador', 'dueño'] 
+let handler = async (m, { conn }) => {
+let vcard = `BEGIN:VCARD
+VERSION:3.0
+N:;SoyMaycol;;;
+FN:SoyMaycol
+ORG:SoyMaycol
+TITLE:Programador
+item1.TEL;waid=51921826291:51921826291
+item1.X-ABLabel:Celular
+item2.EMAIL;type=INTERNET:soymaycol.cn@gmail.com
+item2.X-ABLabel:Correo
+item3.URL:https://github.com/SoySapo6/
+item3.X-ABLabel:GitHub
+X-WA-BIZ-DESCRIPTION:SoyMaycol - Programador joven
+X-WA-BIZ-NAME:SoyMaycol
+END:VCARD`;
 
-export default handler
+    await conn.sendMessage(
+        m.chat, 
+        { contacts: { displayName: 'SoyMaycol', contacts: [{ vcard }] } }, 
+        { quoted: m }
+    );
+};
+
+handler.help = ['owner'];
+handler.tags = ['main'];
+handler.command = ['owner', 'creator', 'creador', 'dueño'];
+
+export default handler;
