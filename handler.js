e731e876a78fcbf5e39360082a844cd8785d67d4
𝐒𝐨𝐲𝕄𝕒𝕪𝕔𝕠𝕝 ᵒᶠⁱᶜⁱᵃˡ <karatekidamericatv@gmail.com>
2025-10-02 23:00:53 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -239,19 +239,25 @@ export async function handler(chatUpdate) {
                 }
             const str2Regex = str => str.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&')
             let _prefix = plugin.customPrefix ? plugin.customPrefix : conn.prefix ? conn.prefix : global.prefix
-            let match = (_prefix instanceof RegExp ? 
-                [[_prefix.exec(m.text), _prefix]] :
-                Array.isArray(_prefix) ?
-                    _prefix.map(p => {
-                        let re = p instanceof RegExp ?
-                            p :
-                            new RegExp(str2Regex(p))
-                        return [re.exec(m.text), re]
-                    }) :
-                    typeof _prefix === 'string' ?
-                        [[new RegExp(str2Regex(_prefix)).exec(m.text), new RegExp(str2Regex(_prefix))]] :
-                        [[[], new RegExp]]
-            ).find(p => p[1])
+let match = (_prefix instanceof RegExp ? 
+    [[_prefix.exec(m.text), _prefix]] :
+    Array.isArray(_prefix) ?
+        _prefix.map(p => {
+            let re = p instanceof RegExp ?
+                p :
+                new RegExp(str2Regex(p))
+            return [re.exec(m.text), re]
+        }) :
+        typeof _prefix === 'string' ?
+            [[new RegExp(str2Regex(_prefix)).exec(m.text), new RegExp(str2Regex(_prefix))]] :
+            [[[], new RegExp]]
+)
+
+if (Array.isArray(match)) {
+    match = match.find(p => p[1])
+} else {
+    match = [[[], new RegExp]]
+}
             if (typeof plugin.before === 'function') {
                 if (await plugin.before.call(this, m, {
                     match,
