67fa21bd6a4df56eba37b1aa765259bf9fac7638
𝐒𝐨𝐲𝕄𝕒𝕪𝕔𝕠𝕝 ᵒᶠⁱᶜⁱᵃˡ <karatekidamericatv@gmail.com>
2025-10-03 00:03:23 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -212,7 +212,7 @@ export async function handler(chatUpdate) {
         const bot = (m.isGroup ? participants.find(u => conn.decodeJid(u.id) == this.user.jid) : {}) || {}
         const isRAdmin = user?.admin == 'superadmin' || false
         const isAdmin = isRAdmin || user?.admin == 'admin' || false
-        const isBotAdmin = bot?.admin || false
+        const isBotAdmin = bot?.admin == 'superadmin' || bot?.admin == 'admin' || false
 
         const ___dirname = path.join(path.dirname(fileURLToPath(import.meta.url)), './plugins')
         for (let name in global.plugins) {
